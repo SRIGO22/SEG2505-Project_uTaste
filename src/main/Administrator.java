a6e133620a3934397d435692f916560a854b7a97
SRIGO22 <srigo059@uottawa.ca>
2025-10-12 02:19:47 -0400

    Create Administrator.java

---
+++ src/main/Administrator.java
@@ -0,0 +1,33 @@
+/**
+* Author: Sara Rigotti
+**/
+
+import java.util.HashMap;
+
+public class Administrator extends User {
+    private HashMap<String, Waiter> waiterAccounts = new HashMap<>();
+
+    public Administrator(String email, String password) {
+        super(email, password);
+    }
+
+    public void createWaiter(String email) {
+        Waiter waiter = new Waiter(email, "waiter-pwd");
+        waiterAccounts.put(email, waiter);
+    }
+
+    public void deleteWaiter(String email) {
+        waiterAccounts.remove(email);
+    }
+
+    public void modifyWaiter(String email, String newFirstName, String newLastName) {
+        Waiter waiter = waiterAccounts.get(email);
+        if (waiter != null) {
+            waiter.updateProfile(newFirstName, newLastName, email);
+        }
+    }
+
+    public String getRole() {
+        return "Administrator";
+    }
+}
