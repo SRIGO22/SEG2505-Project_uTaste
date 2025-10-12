bdccff4cb6741066f7953179e3169472fde111ee
Othmaneelmo <oelmo031@uottawa.ca>
2025-10-12 16:34:09 -0400

    fix gradle building problem using new project files

---
+++ app/src/main/java/com/example/utasteapplication/Chef.java
@@ -0,0 +1,19 @@
+package com.example.utasteapplication;/*
+* Author: Sara Rigotti
+*/
+
+public class Chef extends User implements Role {
+    public Chef(String email, String password) {
+        super(email, password);
+    }
+
+    @Override
+    public String getRole() {
+        return "Chef";
+    }
+
+    @Override
+    public String getRoleName() {
+        return getRole();
+    }
+}
