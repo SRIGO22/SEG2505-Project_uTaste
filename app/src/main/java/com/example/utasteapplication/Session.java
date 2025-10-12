05753a6eedd4664b8da3dfef87cd830ff1a91c9e
Othmaneelmo <oelmo031@uottawa.ca>
2025-10-12 16:24:24 -0400

    fix gradle building problem using new project files

---
+++ app/src/main/java/com/example/utasteapplication/Session.java
@@ -0,0 +1,27 @@
+/*
+* Author: Sara Rigotti
+*/
+
+import java.time.LocalDateTime;
+
+public class Session {
+    private User user;
+    private final String loginTime;
+
+    public Session(User user) {
+        this.user = user;
+        this.loginTime = LocalDateTime.now().toString();
+    }
+
+    public void logout() {
+        this.user = null;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public String getLoginTime() {
+        return loginTime;
+    }
+}
---
+++ app/src/main/java/com/example/utasteapplication/Waiter.java
@@ -0,0 +1,19 @@
+/*
+* Author: Sara Rigotti
+*/
+
+public class Waiter extends User implements Role {
+    public Waiter(String email, String password) {
+        super(email, password);
+    }
+
+    @Override
+    public String getRole() {
+        return "Waiter";
+    }
+
+    @Override
+    public String getRoleName() {
+        return getRole();
+    }
+}
